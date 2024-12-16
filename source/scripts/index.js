93e8c964888229b0abec0638dffa9a83b41fe631
Ivan Shapkov <stfx@live.ru>
2024-12-16 18:10:24 +0300

    Добавляет адаптивную верстку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,21 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+
+navMain.classList.remove('.main-nav--no-js');
+
+navToggle.addEventListener('click', () => {
+  navMain.classList.add('main-nav--close');
+});
+
+// navToggle.addEventListener('click', () => {
+
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--opened');
+//     navMain.classList.remove('main-nav--closed');
+//   }
+// });
