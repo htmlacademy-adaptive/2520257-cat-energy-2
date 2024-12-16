16763d9d00b822abd62c9f37a11ae36f17584ed6
IvanShapkov <IvanShapkov@users.noreply.github.com>
2024-12-16 15:10:56 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2520257-cat-energy-2@51b54db397983fac092b509cb81c118f5d7dbff2 🚀

---
+++ 4/scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav"),e=document.querySelector(".main-nav__toggle");n.classList.remove(".main-nav--no-js");e.addEventListener("click",()=>{n.classList.add("main-nav--close")});
