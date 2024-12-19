780f49fee97d34533fd70db5d191f356dc8cc0af
Ivan Shapkov <stfx@live.ru>
2024-12-19 17:59:51 +0300

    правки в верстку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,20 +2,15 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
-
-navMain.classList.remove('.main-nav--no-js');
+navMain.classList.remove('.main-nav--nojs');
 
 navToggle.addEventListener('click', () => {
-  navMain.classList.add('main-nav--close');
+
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--opened');
+    navMain.classList.remove('main-nav--closed');
+  }
 });
-
-// navToggle.addEventListener('click', () => {
-
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   } else {
-//     navMain.classList.add('main-nav--opened');
-//     navMain.classList.remove('main-nav--closed');
-//   }
-// });
